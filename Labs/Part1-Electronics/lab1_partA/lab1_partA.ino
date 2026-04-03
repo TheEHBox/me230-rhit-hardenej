498fd21a77715f380907a2af363d75407b44c226
rhit-hardenej <hardenej@rose-hulman.edu>
2026-04-03 09:24:25 -0400

    add single button crank

--- Labs/Part1-Electronics/lab1_partA/lab1_partA.ino
+++ Labs/Part1-Electronics/lab1_partA/lab1_partA.ino
@@ -18,13 +18,13 @@ void setup()
   pinMode(LED_BUILTIN, OUTPUT);
   
   for(int i=0; i<numDigital; i++) {
-	pinMode(digitalIns[i], INPUT_PULLUP);
+    pinMode(digitalIns[i], INPUT_PULLUP);
     pinMode(digitalLeds[i], OUTPUT);
     pinMode(digitalOuts[i], OUTPUT);
   }
   
   for(int i=0; i<numAnalog; i++) {
-	//don't need to set analog in, they are always input
+    //don't need to set analog in, they are always input
     pinMode(analogLeds[i], OUTPUT);
     pinMode(analogOuts[i], OUTPUT);
   }
@@ -40,14 +40,14 @@ void loop()
 {
   for(int i=0; i<numDigital; i++) {
     int read = readButton(i);
-	digitalWrite(digitalLeds[i], read);
+    digitalWrite(digitalLeds[i], read);
     digitalWrite(digitalOuts[i], read);
   }
   
   for(int i=0; i<numAnalog; i++) {
     int read = analogRead(analogIns[i])/4; //div by 4 to match the scales
     //analog read is [0,1024) and analog write is [0,256)
-	//don't need to set analog in, they are always input
+    //don't need to set analog in, they are always input
     analogWrite(analogLeds[i], read);
     analogWrite(analogOuts[i], read);
   }
--- proj/proj.ino
+++ proj/proj.ino
@@ -1,38 +1,61 @@
 // C++ code
 //
 
-#define PIN_CRANK_MID 0 //analog 0.  motor->diode->resistor->resistor-(here)>resistor-(ground)>resistor->diode->motor
-#define PIN_CRANK_HIGH 1 //analog 1. motor->diode->resistor-(here)>resistor->resistor-(ground)>resistor->diode->motor
+#define PIN_DYNAMO_CRANK_MID 0 //analog 0.  motor->diode->resistor->resistor-(here)>resistor-(ground)>resistor->diode->motor
+#define PIN_DYNAMO_CRANK_HIGH 1 //analog 1. motor->diode->resistor-(here)>resistor->resistor-(ground)>resistor->diode->motor
+
+#define PIN_SINGLE_BUTTON_CRANK 2 //digital 1. simple button that gets pressed by a cam connected to the crank
 
 void setup()
 {
   Serial.begin(9600);
+
+  pinMode(PIN_SINGLE_BUTTON_CRANK, INPUT_PULLUP);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
+
+
+//true if pressed, false if not
+int readButton(int pin) {
+	return !digitalRead(pin);
+}
+
+
+
+
+static constexpr int LOOP_TIME = 300; // how long the main processor waits between loops
+unsigned long lastLoopTime = 0;
 
 void loop()
 {
+
+  if(millis()-lastLoopTime>LOOP_TIME){
+    lastLoopTime = millis();
+
+
+
+    //printRaw();
+    Serial.println(getSigned());
+  }
   
-  //printRaw();
-  
-  Serial.println(getSigned());
-  
-  delay(100);
+
+  digitalWrite(LED_BUILTIN, readButton(PIN_SINGLE_BUTTON_CRANK));
 }
 
 
 int getSigned() {
-  int a0 = analogRead(0);  
-  int a1 = analogRead(1);
-  int d = a0-a1;
-  if(d<0) d = -d;
+  int mid = analogRead(PIN_DYNAMO_CRANK_MID);  
+  int high = analogRead(PIN_DYNAMO_CRANK_HIGH);
+  int diff = mid-high;
+  if(diff<0) diff = -diff; //absolute value
   
-  if(d<5) return a1; //if they are close, then reading a positive voltage
-  return -a1; //if they are not close (a0 is close to 0) then it is negative
+  if(diff<5) return high; //if they are close, then reading a positive voltage
+  return -high; //if they are not close (mid is close to 0) then it is negative
 }
 
 void printRaw() {
-  Serial.print("a0: ");
-  Serial.print(analogRead(0));
-  Serial.print("\ta1: ");
-  Serial.println(analogRead(1));
+  Serial.print("mid: ");
+  Serial.print(analogRead(PIN_DYNAMO_CRANK_MID));
+  Serial.print("\thigh: ");
+  Serial.println(analogRead(PIN_DYNAMO_CRANK_HIGH));
 }
